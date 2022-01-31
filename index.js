c91e488610cbc6ecb5eedf0663f6970faf829384
SiriusKoan <k.peihsun@gmail.com>
2022-01-31 14:21:49 +0800

    Finish time logic

---
+++ index.js
@@ -0,0 +1,54 @@
+function month_days(month, year) {
+    var days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (month == 2) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return 29;
+                }
+                return 28;
+            }
+            return 29;
+        }
+        return 28;
+    }
+    return days[month - 1];
+}
+
+function year_days(year) {
+    var days = 0;
+    for (var i = 1; i <= 12; i++) {
+        days += month_days(i, year);
+    }
+    return days;
+}
+
+year_progress_bar = document.getElementById("year").children[0];
+month_progress_bar = document.getElementById("month").children[0];
+day_progress_bar = document.getElementById("day").children[0];
+
+function render_progress_bar() {
+    var now = new Date();
+    // year
+    var seconds_of_passed_months = 0;
+    for (var i = 1; i < now.getMonth(); i++) {
+        seconds_of_passed_months += month_days(i, now.getFullYear()) * 86400;
+    }
+    seconds_of_passed_months += now.getDate() * 86400;
+    seconds_of_passed_months += now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600
+    var year_percentage = seconds_of_passed_months * 100 / (year_days(now.getFullYear()) * 86400);
+    year_progress_bar.style.width = year_percentage + "%";
+    year_progress_bar.innerText = year_percentage.toFixed(2) + "%";
+    // month
+    var seconds_of_passed_days = 86400 * (now.getDate() - 1);
+    seconds_of_passed_days += now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600
+    var month_percentage = seconds_of_passed_days * 100 / (month_days(now.getMonth() + 1, now.getFullYear()) * 86400);
+    month_progress_bar.style.width = month_percentage + "%";
+    month_progress_bar.innerText = month_percentage.toFixed(2) + "%";
+    // day
+    var day_percentage = (now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600) * 100 / 86400;
+    day_progress_bar.style.width = day_percentage + "%";
+    day_progress_bar.innerText = day_percentage.toFixed(2) + "%";
+}
+
+render_progress_bar();
