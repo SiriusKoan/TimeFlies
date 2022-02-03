2fd3ddaa4e41b4dd2e9e465e2a9c1225f085ec19
SiriusKoan <k.peihsun@gmail.com>
2022-02-03 10:36:00 +0800

    Fix year progress problem and var

--- index.js
+++ index.js
@@ -18,58 +18,58 @@ function month_days(month, year) {
 }
 
 function year_days(year) {
-    var days = 0;
-    for (var i = 1; i <= 12; i++) {
+    let days = 0;
+    for (let i = 1; i <= 12; i++) {
         days += month_days(i, year);
     }
     return days;
 }
 
 function render_current_time() {
-    var now = new Date();
-    var hours = `${now.getHours()}`.padStart(2, "0");
-    var minutes = `${now.getMinutes()}`.padStart(2, "0");
-    var seconds = `${now.getSeconds()}`.padStart(2, "0");
-    var day = `${now.getDate()}`.padStart(2, "0");
-    var month = `${now.getMonth() + 1}`.padStart(2, "0");
-    var year = now.getFullYear();
-    var day_of_week = now.getDay();
+    let now = new Date();
+    let hours = `${now.getHours()}`.padStart(2, "0");
+    let minutes = `${now.getMinutes()}`.padStart(2, "0");
+    let seconds = `${now.getSeconds()}`.padStart(2, "0");
+    let day = `${now.getDate()}`.padStart(2, "0");
+    let month = `${now.getMonth() + 1}`.padStart(2, "0");
+    let year = now.getFullYear();
+    let day_of_week = now.getDay();
     document.getElementById("current-time").innerText = `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${day_of_week_names[day_of_week]}`;
     setTimeout(render_current_time, 200);
 }
 
 function generate_ticks(id, count) {
-    var bar = document.getElementById(id);
-    var ticks_html = "";
-    for (var i = 1; i < count; i++) {
+    let bar = document.getElementById(id);
+    let ticks_html = "";
+    for (let i = 1; i < count; i++) {
         ticks_html += `<span class="tick" style="left: ${i * 100 / count}%"></span>`;
     }
     bar.innerHTML += ticks_html;
 }
 
 function render_progress_bar() {
-    var now = new Date();
-    var year_progress_bar = document.getElementById("year").children[0];
-    var month_progress_bar = document.getElementById("month").children[0];
-    var day_progress_bar = document.getElementById("day").children[0];
+    let now = new Date();
+    let year_progress_bar = document.getElementById("year").children[0];
+    let month_progress_bar = document.getElementById("month").children[0];
+    let day_progress_bar = document.getElementById("day").children[0];
     // year
-    var seconds_of_passed_months = 0;
-    for (var i = 1; i < now.getMonth(); i++) {
+    let seconds_of_passed_months = 0;
+    for (let i = 1; i < now.getMonth() + 1; i++) {
         seconds_of_passed_months += month_days(i, now.getFullYear()) * 86400;
     }
-    seconds_of_passed_months += now.getDate() * 86400;
-    seconds_of_passed_months += now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600
-    var year_percentage = seconds_of_passed_months * 100 / (year_days(now.getFullYear()) * 86400);
+    seconds_of_passed_months += (now.getDate() - 1) * 86400;
+    seconds_of_passed_months += now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600;
+    let year_percentage = seconds_of_passed_months * 100 / (year_days(now.getFullYear()) * 86400);
     year_progress_bar.style.width = year_percentage.toFixed(2) + "%";
     year_progress_bar.innerText = year_percentage.toFixed(2) + "%";
     // month
-    var seconds_of_passed_days = 86400 * (now.getDate() - 1);
+    let seconds_of_passed_days = 86400 * (now.getDate() - 1);
     seconds_of_passed_days += now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600
-    var month_percentage = seconds_of_passed_days * 100 / (month_days(now.getMonth() + 1, now.getFullYear()) * 86400);
+    let month_percentage = seconds_of_passed_days * 100 / (month_days(now.getMonth() + 1, now.getFullYear()) * 86400);
     month_progress_bar.style.width = month_percentage.toFixed(2) + "%";
     month_progress_bar.innerText = month_percentage.toFixed(2) + "%";
     // day
-    var day_percentage = (now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600) * 100 / 86400;
+    let day_percentage = (now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600) * 100 / 86400;
     day_progress_bar.style.width = day_percentage.toFixed(2) + "%";
     day_progress_bar.innerText = day_percentage.toFixed(2) + "%";
     setTimeout(render_progress_bar, 1000);
