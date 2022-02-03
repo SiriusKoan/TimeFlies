7bbaaba5c28c65b51a06ebf686697a356e69a726
SiriusKoan <k.peihsun@gmail.com>
2022-02-03 11:11:08 +0800

    Fix tick override progress problem

--- index.js
+++ index.js
@@ -49,10 +49,18 @@ function generate_ticks(id, count) {
 
 function render_progress_bar() {
     let now = new Date();
-    let year_progress_bar = document.getElementById("year").children[0];
-    let month_progress_bar = document.getElementById("month").children[0];
-    let week_progress_bar = document.getElementById("week").children[0];
-    let day_progress_bar = document.getElementById("day").children[0];
+    let year_parent = document.getElementById("year");
+    let year_progress = year_parent.children[0];
+    let year_progress_bar = year_parent.children[1];
+    let month_parent = document.getElementById("month");
+    let month_progress = month_parent.children[0];
+    let month_progress_bar = month_parent.children[1];
+    let week_parent = document.getElementById("week");
+    let week_progress = week_parent.children[0];
+    let week_progress_bar = week_parent.children[1];
+    let day_parent = document.getElementById("day");
+    let day_progress = day_parent.children[0];
+    let day_progress_bar = day_parent.children[1];
     // year
     let seconds_of_passed_months = 0;
     for (let i = 1; i < now.getMonth() + 1; i++) {
@@ -62,29 +70,29 @@ function render_progress_bar() {
     seconds_of_passed_months += now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600;
     let year_percentage = seconds_of_passed_months * 100 / (year_days(now.getFullYear()) * 86400);
     year_progress_bar.style.width = year_percentage.toFixed(2) + "%";
-    year_progress_bar.innerText = year_percentage.toFixed(2) + "%";
+    year_progress.innerText = year_percentage.toFixed(2) + "%";
     // month
     let seconds_of_passed_days = 86400 * (now.getDate() - 1);
     seconds_of_passed_days += now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600
     let month_percentage = seconds_of_passed_days * 100 / (month_days(now.getMonth() + 1, now.getFullYear()) * 86400);
     month_progress_bar.style.width = month_percentage.toFixed(2) + "%";
-    month_progress_bar.innerText = month_percentage.toFixed(2) + "%";
+    month_progress.innerText = month_percentage.toFixed(2) + "%";
     // week
     let seconds_of_passed_days_of_week = 86400 * (now.getDay());
     seconds_of_passed_days_of_week += now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600;
     let week_percentage = seconds_of_passed_days_of_week * 100 / (86400 * 7);
     week_progress_bar.style.width = week_percentage.toFixed(2) + "%";
-    week_progress_bar.innerText = week_percentage.toFixed(2) + "%";
+    week_progress.innerText = week_percentage.toFixed(2) + "%";
     // day
     let day_percentage = (now.getSeconds() + now.getMinutes() * 60 + now.getHours() * 3600) * 100 / 86400;
     day_progress_bar.style.width = day_percentage.toFixed(2) + "%";
-    day_progress_bar.innerText = day_percentage.toFixed(2) + "%";
+    day_progress.innerText = day_percentage.toFixed(2) + "%";
     setTimeout(render_progress_bar, 1000);
 }
 
 render_progress_bar();
 render_current_time();
-generate_ticks("year", 4);
+generate_ticks("year", 12);
 generate_ticks("month", 4);
-generate_ticks("week", 2);
+generate_ticks("week", 7);
 generate_ticks("day", 2);
